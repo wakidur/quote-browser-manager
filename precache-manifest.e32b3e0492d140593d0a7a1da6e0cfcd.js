4971f72f5cbc7b60fd1f870022529479bdcf5d96
wakidur <wakidur@gmail.com>
2020-07-22 13:41:32 +0600

    Updates

---
+++ precache-manifest.e32b3e0492d140593d0a7a1da6e0cfcd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e6e50dabfa8a7b48f1858d67d30b58",
+    "url": "/quote-browser-manager/index.html"
+  },
+  {
+    "revision": "586f852fba8615eaf4ab",
+    "url": "/quote-browser-manager/static/css/2.98837b08.chunk.css"
+  },
+  {
+    "revision": "f3af326d9fa8f400d86e",
+    "url": "/quote-browser-manager/static/css/main.3a6fd054.chunk.css"
+  },
+  {
+    "revision": "586f852fba8615eaf4ab",
+    "url": "/quote-browser-manager/static/js/2.0bc81400.chunk.js"
+  },
+  {
+    "revision": "ba9f1cbbd5250a36f31fb5b559f6f707",
+    "url": "/quote-browser-manager/static/js/2.0bc81400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3af326d9fa8f400d86e",
+    "url": "/quote-browser-manager/static/js/main.aa6c027c.chunk.js"
+  },
+  {
+    "revision": "d6e5fb296898c9ed20f3",
+    "url": "/quote-browser-manager/static/js/runtime-main.8f434435.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quote-browser-manager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/quote-browser-manager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quote-browser-manager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quote-browser-manager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quote-browser-manager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
